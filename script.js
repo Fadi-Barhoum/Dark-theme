27b00547054ddc62bbd432bda0ed5654e6339d03
Fadi Barhoum <fadi.barhoum.web@gmail.com>
2023-09-28 16:24:00 +0300

    save theme

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const icon = document.getElementById("icon");
+
+if (localStorage.getItem("theme")== null){
+    localStorage.setItem("theme","light");
+}
+
+let local = localStorage.getItem("theme");
+
+if (local == "light"){
+    icon.src = "images/moon.png";
+    document.body.classList.remove("dark-theme");
+}else if (local == "dark"){
+    icon.src = "images/sun.png";
+    document.body.classList.add("dark-theme");
+}
+
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")){
         icon.src = "images/sun.png";
+        localStorage.setItem("theme","dark");
     }else{
         icon.src ="images/moon.png";
+        localStorage.setItem("theme","light");
     }
 }
